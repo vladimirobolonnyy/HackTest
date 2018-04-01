08bec3c3502e2e6c4d23b3d2ce87ba9faadd7f62
Obolonnyy Vladimir <obolonnyyv@gmail.com>
2018-04-01 20:48:34 +0300

    first screen

--- app/src/main/java/com/obolonnyy/hacktest/MainActivity.kt
+++ app/src/main/java/com/obolonnyy/hacktest/MainActivity.kt
@@ -2,11 +2,25 @@ package com.obolonnyy.hacktest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        mainRecyclerView.adapter = ParticipantsAdapter(generateUs())
+    }
+
+    fun generateUs(): ArrayList<Participant> {
+        val result = ArrayList<Participant>()
+        for (i in 0..5){
+            result.add(Participant("${i}", "${i+10}"))
+            println(i)
+        }
+        return result
     }
 }
---
+++ app/src/main/java/com/obolonnyy/hacktest/Participant.kt
@@ -0,0 +1,14 @@
+package com.obolonnyy.hacktest
+
+/**
+ * Created by Vova on 01.04.2018.
+ */
+data class Participant(
+        var firstName: String = "",
+        var lastName: String = "",
+        var middleName: String = "",
+        var sex: String = "",
+        var group: String = "",
+        var photopath: String = "",
+        var description: String = ""
+)
