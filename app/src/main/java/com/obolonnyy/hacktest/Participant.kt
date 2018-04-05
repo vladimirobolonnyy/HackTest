6a7ae24a86031c8790eabec5a728608fed76bbb2
GitWhiskey <max.khabrat@gmail.com>
2018-04-05 23:26:04 +0300

    Model change

--- app/src/main/java/com/obolonnyy/hacktest/Participant.kt
+++ app/src/main/java/com/obolonnyy/hacktest/Participant.kt
@@ -1,17 +1,17 @@
 package com.obolonnyy.hacktest
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
 /**
  * Created by Vova on 01.04.2018.
  */
 open class Participant (
-        var id: String = UUID.randomUUID().toString(),
+        @PrimaryKey var id: String = UUID.randomUUID().toString(),
         var firstName: String = "",
         var lastName: String = "",
         var middleName: String = "",
-        var sex: String = "",
         var group: String = "",
         var photopath: String = "",
         var description: String = ""
